e6c5a4346085db4ca955f3b6d7a97df0b8663716
ben1sheff <ben1sheff@berkeley.edu>
2024-06-26 19:46:08 -0600

    Remove bouncing ball and add a little mask to just show a yellow object

--- camera_test.py
+++ camera_test.py
@@ -5,7 +5,8 @@ import time
 import numpy as np
 
 # Params
-dispWH = (1280, 720) # Should be able to go to 2592x1944. Others: 1296, 972; 640 x 480 also works. Use standard sizes
+dispW = 1280
+dispH = 720 # Should be able to go to 2592x1944. Others: 1296, 972; 640 x 480 also works. Use standard sizes
 # FPS tracker details:
 font_size = 0.6
 font_pos = (30, 60) # Distance from top left in pixels
@@ -14,7 +15,7 @@ font_weight = 2
 
 button = Button(18)
 picam = Picamera2()
-picam.preview_configuration.main.size = dispWH
+picam.preview_configuration.main.size = (dispW, dispH)
 picam.preview_configuration.main.format = "RGB888"
 picam.preview_configuration.controls.FrameRate = 30
 picam.preview_configuration.align()  # Forces to standard size for speed
@@ -26,21 +27,39 @@ picam.start()
 # box_LR = (750, 125)
 # box_color = (255, 0, 255)
 # box_thick = 7 # Line width, -1 means solid
+
 # And a circle
 circle_cent = [900, 500]
 circle_color = (155, 0, 155)
 circle_thick = -1
 circle_rad = 35
 circle_v = [30, 80]
+trap_flag = [False, False]
 def update_circle(center, vel, rad, edges, dt):
     new_center = center.copy()  # [center[0] + vel[0] * dt, center[1] + vel[1] * dt]
     new_vel = vel.copy()
     for i in range(len(new_center)):
         new_center[i] += vel[i] * dt
-        if new_center[i] < rad or new_center[i] > edges[i] - rad:
-            new_vel[i] = -vel[i]
-            # new_center[i] += 2 * new_vel[i] * dt  # Prevent overshoot
+        if new_center[i] < rad or new_center[i] >= edges[i] - rad:
+            if not trap_flag[i]:
+                new_vel[i] = -vel[i]
+                trap_flag[i] = True
+                # new_center[i] += 2 * new_vel[i] * dt  # Prevent overshoot
+        else:
+            trap_flag[i] = False
+
     return new_center, new_vel
+ 
+# Let's make a little ROI
+xRange = [dispW//4, 3*dispW//4]
+yRange = [dispH//4, 3*dispH//4]
+
+# How about searching an image based on color?
+hueRange = [20, 30]
+satRange = [100, 255]
+valRange = [100, 255]
+mask_low = np.array([hueRange[0], satRange[0], valRange[0]])
+mask_high = np.array([hueRange[1], satRange[1], valRange[1]])
 
 # Loop setup stuff
 exit_flag = True
@@ -54,7 +73,8 @@ def leave_loop():
 button.when_pressed = leave_loop
 
 while exit_flag:
-    image = picam.capture_array()  # Array of pixels, [row(width)][col(height)][B,G,R]
+    image = picam.capture_array()  # Array of pixels, [col(height)][row(width)][B,G,R]
+    imageHSV = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)  # Convert to HSV for human intuition
 
     # Add frame rate
     timer2 = time.time()
@@ -68,10 +88,21 @@ while exit_flag:
     # Add a rectangle
     # cv2.rectangle(image, box_UL, box_LR, box_color, box_thick)
 
-    circle_cent, circle_v = update_circle(circle_cent,
-                                          circle_v, circle_rad, dispWH, dt)
-    cv2.circle(image, (int(circle_cent[0]), int(circle_cent[1])),
-                       circle_rad, circle_color, circle_thick)
+    # Add a circle
+    # circle_cent, circle_v = update_circle(circle_cent,circle_v, circle_rad,
+    #                                       [dispW, dispH], dt)
+    # cv2.circle(image, (int(circle_cent[0]), int(circle_cent[1])),
+    #                    circle_rad, circle_color, circle_thick)
+
+    # # make and separately display ROI
+    # roi = image[yRange[0]:yRange[1], xRange[0]:xRange[1]]
+    # cv2.imshow("ROI", roi)
+
+    # make a mask that's white only for points in the color range we've set
+    mask = cv2.inRange(imageHSV, mask_low, mask_high)
+    obj_of_interest = cv2.bitwise_and(image, image, mask=mask)
+    small = cv2.resize(obj_of_interest, (dispW//2, dispH//2))
+    cv2.imshow("Mask", small)
 
     # Display and exit possibility
     cv2.imshow("picam", image)
