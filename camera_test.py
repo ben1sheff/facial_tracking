8ecb9dd51580752859c61d006846c1577e347347
ben1sheff <ben1sheff@berkeley.edu>
2024-06-26 21:09:00 -0600

    Added sliders to tune the mask to an object, draw a contour around the largest matching object, and a box around that.

--- camera_test.py
+++ camera_test.py
@@ -35,31 +35,60 @@ circle_thick = -1
 circle_rad = 35
 circle_v = [30, 80]
 trap_flag = [False, False]
-def update_circle(center, vel, rad, edges, dt):
-    new_center = center.copy()  # [center[0] + vel[0] * dt, center[1] + vel[1] * dt]
-    new_vel = vel.copy()
-    for i in range(len(new_center)):
-        new_center[i] += vel[i] * dt
-        if new_center[i] < rad or new_center[i] >= edges[i] - rad:
-            if not trap_flag[i]:
-                new_vel[i] = -vel[i]
-                trap_flag[i] = True
-                # new_center[i] += 2 * new_vel[i] * dt  # Prevent overshoot
-        else:
-            trap_flag[i] = False
+# def update_circle(center, vel, rad, edges, dt):
+#     new_center = center.copy()  # [center[0] + vel[0] * dt, center[1] + vel[1] * dt]
+#     new_vel = vel.copy()
+#     for i in range(len(new_center)):
+#         new_center[i] += vel[i] * dt
+#         if new_center[i] < rad or new_center[i] >= edges[i] - rad:
+#             if not trap_flag[i]:
+#                 new_vel[i] = -vel[i]
+#                 trap_flag[i] = True
+#                 # new_center[i] += 2 * new_vel[i] * dt  # Prevent overshoot
+#         else:
+#             trap_flag[i] = False
 
-    return new_center, new_vel
+#     return new_center, new_vel
  
 # Let's make a little ROI
 xRange = [dispW//4, 3*dispW//4]
 yRange = [dispH//4, 3*dispH//4]
 
 # How about searching an image based on color?
-hueRange = [20, 30]
-satRange = [100, 255]
-valRange = [100, 255]
-mask_low = np.array([hueRange[0], satRange[0], valRange[0]])
-mask_high = np.array([hueRange[1], satRange[1], valRange[1]])
+hue_range = [12, 26]
+sat_range = [180, 255]
+val_range = [10, 255]
+mask_low = lambda : np.array([hue_range[0], sat_range[0], val_range[0]])
+mask_high = lambda : np.array([hue_range[1], sat_range[1], val_range[1]])
+
+# Make track bars for better color searches
+cv2.namedWindow("Trackbars")
+def hue_low(val):
+    global hue_range
+    hue_range[0] = val
+def hue_high(val):
+    global hue_range
+    hue_range[1] = val
+def sat_low(val):
+    global sat_range
+    sat_range[0] = val
+def sat_high(val):
+    global sat_range
+    sat_range[1] = val
+def val_low(val):
+    global val_range
+    val_range[0] = val
+def val_high(val):
+    global val_range
+    val_range[1] = val
+cv2.createTrackbar("Hue Min", "Trackbars", hue_range[0], 179, hue_low)
+cv2.createTrackbar("Hue Max", "Trackbars", hue_range[1], 179, hue_high)
+cv2.createTrackbar("Saturation Min", "Trackbars", sat_range[0], 255, sat_low)
+cv2.createTrackbar("Saturation Max", "Trackbars", sat_range[1], 255, sat_high)
+cv2.createTrackbar("Value Min", "Trackbars", val_range[0], 255, val_low)
+cv2.createTrackbar("Value Max", "Trackbars", val_range[1], 255, val_high)
+
+
 
 # Loop setup stuff
 exit_flag = True
@@ -98,11 +127,21 @@ while exit_flag:
     # roi = image[yRange[0]:yRange[1], xRange[0]:xRange[1]]
     # cv2.imshow("ROI", roi)
 
-    # make a mask that's white only for points in the color range we've set
-    mask = cv2.inRange(imageHSV, mask_low, mask_high)
+    # make a mask that's white only for points in the color range we've set, then cut out black areas
+    mask = cv2.inRange(imageHSV, mask_low(), mask_high()) //255
     obj_of_interest = cv2.bitwise_and(image, image, mask=mask)
     small = cv2.resize(obj_of_interest, (dispW//2, dispH//2))
     cv2.imshow("Mask", small)
+
+    # Make contours out of the masks
+    contours, junk = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    if len(contours) > 0:
+        biggest_contour_ind = np.argmax([cv2.contourArea(contour) for contour in contours])
+        # contours = sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)
+        biggest_contour = contours[biggest_contour_ind]
+        cv2.drawContours(image, [biggest_contour], 0, (255, 0, 0), 3)
+        x, y, w, h = cv2.boundingRect(biggest_contour)
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255))
 
     # Display and exit possibility
     cv2.imshow("picam", image)
